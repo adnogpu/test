860c907c660140460e95ffffc28013038e91d543
adnogpu <86664074+adnogpu@users.noreply.github.com>
2024-08-07 19:37:14 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://fqnovel.pages.dev/";//your website url
+  let target = "http://47.106.111.98/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
