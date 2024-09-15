bdaf8a3d4a3df0983a8c112e5ec33bc891f609a2
adnogpu <86664074+adnogpu@users.noreply.github.com>
2024-09-15 18:08:02 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://api.mcstatus.io";//your website url
+  let target = "http://110.42.33.87:2173";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
